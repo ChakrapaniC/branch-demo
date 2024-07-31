e4d7f39a69cedc253345ea812f780e6756e0abb1
Chakrapani <chitranshsharma53@gmail.com>
2024-07-31 19:03:30 +0530

    new feature is added

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 const multiply = (a , b) => {
     return a * b;
 }
-console.log(multiply(4,5))
+console.log(multiply(4,5)) ; 
+
+//feature 1 code
+const removeDupl = (arr) =>{
+    let result = [];
+    for(let i=0; i<arr.length ; i++){
+        let dupl = false;
+        for(let j=0; j<result.length ; j++){
+           if(arr[i] === result[j]){
+              dupl = true
+              break;
+           }
+        }
+        if(!dupl){
+            result.push(arr[i]);
+        }
+    }
+    return result
+}
+const arrray = [1,2,3,4,2];
+console.log(removeDupl(arrray));
